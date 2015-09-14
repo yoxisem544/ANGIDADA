854353e730343d6b0e5e579cec3a50d1a0dd8f32
David <yoxisem544@gmail.com>
2015-09-14 12:34:21 +0800

    Initial Commit

---
+++ AnnGiNoCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnnGiNoCoreData
+//
+//  Created by David on 2015/9/14.
+//  Copyright (c) 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
