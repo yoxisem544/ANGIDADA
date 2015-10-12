f4187bcaadd5ece3a7856c060aea43293db84254
David <yoxisem544@gmail.com>
2015-10-12 06:34:30 -0700

    setup storyboard

---
+++ AnnGiNoCoreData/morning/MorningFirstComfirmViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MorningFirstComfirmViewController.swift
+//  AnnGiNoCoreData
+//
+//  Created by David on 2015/10/11.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class MorningFirstComfirmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AnnGiNoCoreData/night/NegativeAffect4ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  Authoritarian2ViewController.swift
+//  AnnGiNoCoreData
+//
+//  Created by David on 2015/9/16.
+//  Copyright (c) 2015年 David. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class NegativeAffect4ViewController: UIViewController {
+    
+    
+    let fearLevel = ["從未如此","很少如此","偶爾如此","有時如此","經常如此","總是如此"]
+    var user: PersonalInformation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad(); self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.jpg")!)
+        
+        title = "初始問卷 (70/72)"
+        var backbutton = UIBarButtonItem(title: "70/72", style: UIBarButtonItemStyle.Done, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem = backbutton
+        // Do any additional setup after loading the view.
+        fearLevelPickerView.selectRow(0, inComponent: 0, animated: true)
+        user.chasteningNormOrientationQuestion5 = fearLevel[0]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var fearLevelPickerView: UIPickerView!
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "next" {
+            let vc = segue.destinationViewController as! NegativeAffect5ViewController
+            vc.user = user
+        }
+    }
+    
+    
+    @IBAction func nextClicked(sender: AnyObject) {
+        performSegueWithIdentifier("next", sender: user)
+    }
+}
+
+extension NegativeAffect4ViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return fearLevel[row]
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fearLevel.count
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(fearLevel[row])
+        user.chasteningNormOrientationQuestion5 = fearLevel[row]
+    }
+}
